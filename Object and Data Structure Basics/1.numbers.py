9f20a142b770f2bf3d52ebadfaadb7872fdab4fb
RodrigoBaltazar <rodrigo@MacBook-Pro-de-RodrigoBaltazar.local>
2022-04-30 21:16:55 -0300

    add first commit

---
+++ Object and Data Structure Basics/1.numbers.py
@@ -0,0 +1,24 @@
+
+
+## Operador MOD
+
+resto = 7 % 4
+# resposta 3 + 4 == 7 então:
+# resto == 3
+
+resto_zero = 50 % 5 # Se o reto for zero temos um número par.
+
+resto_zero = 23 % 2 # Se numero % 2 der diferente de 0 sabemos que temos um número impar.
+
+## Potencias
+
+potencia = 2 ** 3
+
+## Parenteses de predecencia
+
+com_parenteses = (2 + 10) * (10 + 3)
+# 2 + 10 = 12
+# *
+# 10 + 3 = 13
+# 12 * 13
+# resultado: 156
---
+++ Object and Data Structure Basics/2.variable_assignments.py
@@ -0,0 +1,12 @@
+## Dynamic Typing
+    
+    
+    #CONS
+    # Need to be aware of type()
+
+my_cat = 1
+
+my_cat = 'Fi'
+
+print (my_cat + my_cat)
+print(type(my_cat))
